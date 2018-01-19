20b8d21b278db638d0c61ee749ceb4cdb6f33dc5
Unknown <mookmook02na@gmail.com>
2018-01-19 13:39:53 +0700

    แก้

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -11,6 +11,27 @@ int main () {
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
 
+        char mook[8];
+
+        mook[0]=name [6];
+        mook[1]=name [5];
+        mook[2]=name [4];
+        mook[3]=name [3];
+        mook[4]=name [2];
+        mook[5]=name [1];
+        mook[6]=name [0];
+        mook[7]='\0';
+        cout << mook endl;
+
+        mook[0]= mook[0] +1;
+        mook[1]= mook[1] +1;
+        mook[2]= mook[2] +1;
+        mook[3]= mook[3] +1;
+        mook[4]= mook[4] +1;
+        mook[5]= mook[5] +1;
+        mook[6]= mook[6] +1;
+        cout << mook endl;
+
 
 
         return 0;
